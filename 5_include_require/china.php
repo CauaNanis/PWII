205a28417bd5143fbfe87aab3e9b100c5d9d8a72
CauaNanis <caua.nanis.rosa2009@gmail.com>
2025-03-13 18:25:31 -0300

    Aula 13/03

---
+++ 5_include_require/china.php
@@ -0,0 +1,26 @@
+<?php include './header.php';?>
+
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-itens-center">
+                <h1>Muralha da China</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://100grana.wordpress.com/wp-content/uploads/2009/12/df-27222_rtop.jpg?w=584" style= "width 100%">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://100grana.wordpress.com/wp-content/uploads/2009/12/df-27222_rtop.jpg?w=584" style= "width 100%">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<?php include './footer.php';?>
